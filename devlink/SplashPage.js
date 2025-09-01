d4e74f376fbfd0ec26448c779a5e4ac9bed402b7
Brent Hegnauer <brent.hegnauer@outlook.com>
2025-09-01 15:59:35 -0400

    Splash typo

--- devlink/SplashPage.js
+++ devlink/SplashPage.js
@@ -7,6 +7,17 @@ import _styles from "./SplashPage.module.css";
 export function SplashPage({ as: _Component = _Builtin.Block }) {
   return (
     <_Component className={_utils.cx(_styles, "div-block")} tag="div">
+      <_Builtin.BackgroundVideoWrapper
+        className={_utils.cx(_styles, "splash-background")}
+        tag="div"
+        sources={[
+          "https://cdn.prod.website-files.com/68b3d866747a7ae841e33100%2F68b45619f2bef3c9560fc500_green-transcode.webm",
+          "https://cdn.prod.website-files.com/68b3d866747a7ae841e33100%2F68b45619f2bef3c9560fc500_green-transcode.mp4",
+        ]}
+        posterImage="https://cdn.prod.website-files.com/68b3d866747a7ae841e33100%2F68b45619f2bef3c9560fc500_green-poster-00001.jpg"
+        autoPlay={true}
+        loop={true}
+      />
       <_Builtin.Block className={_utils.cx(_styles, "div-block-2")} tag="div">
         <_Builtin.NavbarWrapper
           className={_utils.cx(_styles, "navbar")}
@@ -63,17 +74,6 @@ export function SplashPage({ as: _Component = _Builtin.Block }) {
           </_Builtin.Block>
         </_Builtin.BlockContainer>
       </_Builtin.Block>
-      <_Builtin.BackgroundVideoWrapper
-        className={_utils.cx(_styles, "splash-background")}
-        tag="div"
-        sources={[
-          "https://cdn.prod.website-files.com/68b3d866747a7ae841e33100%2F68b45619f2bef3c9560fc500_green-transcode.webm",
-          "https://cdn.prod.website-files.com/68b3d866747a7ae841e33100%2F68b45619f2bef3c9560fc500_green-transcode.mp4",
-        ]}
-        posterImage="https://cdn.prod.website-files.com/68b3d866747a7ae841e33100%2F68b45619f2bef3c9560fc500_green-poster-00001.jpg"
-        autoPlay={true}
-        loop={true}
-      />
     </_Component>
   );
 }
